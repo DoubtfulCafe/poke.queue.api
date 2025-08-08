672fa4b015247d72a44148cdd51657c2d1708259
Daniel Villeda <dvilleda2007@gmail.com>
2025-08-08 17:49:15 -0600

    feat: guardar requests en DB y enviar a Azure Queue

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 import uvicorn
 import json
 import os
-from utils.database import execute_query_json
 from fastapi import FastAPI
+from utils.database import execute_query_json
+from controllers.PokeRequestController import insert_pokemon_request
+from models.PokeRequest import PokemonRequest
 
-app= FastAPI()
+app = FastAPI()
 
 @app.get("/")
 async def root():
-    query = "SELECT * FROM pokequeue.MESSAGES"
+    query = "select * from pokequeue.MESSAGES"
     result = await execute_query_json(query)
     result_dict = json.loads(result)
     return result_dict
 
 @app.get("/api/version")
 async def version():
-    return { "version": "0.0.0" }
+    return { "version": "0.1.0" }
 
+@app.post("/api/request")
+async def create_request(pokemon_request: PokemonRequest):
+    return await insert_pokemon_request(pokemon_request)
 
-if __name__=="__main__":
+if __name__ == "__main__":
+    import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
