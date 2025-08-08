34e27db681602731ac9d577bc37443855f6996fd
Daniel Villeda <dvilleda2007@gmail.com>
2025-08-08 13:32:57 -0600

    feat/db: conecta base de datos y muestra mensaje de bienvenida
    Se estableció la conexión con la base de datos, se crearon las tablas necesarias y se configuró una ruta para obtener un mensaje de bienvenida directamente desde la base.

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import uvicorn
-
+import json
+import os
+from utils.database import execute_query_json
 from fastapi import FastAPI
 
 app= FastAPI()
 
 @app.get("/")
 async def root():
-    return { "project": "POKE QUEUE REQUEST" }
+    query = "SELECT * FROM pokequeue.MESSAGES"
+    result = await execute_query_json(query)
+    result_dict = json.loads(result)
+    return result_dict
 
 @app.get("/api/version")
 async def version():
